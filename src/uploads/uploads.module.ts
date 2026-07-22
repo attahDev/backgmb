fd222629f2e30115e5f0af639961b8e09bc667f2
attahDev <attahumana007@gmail.com>
2026-07-22 17:07:36 +0100

    Add PrismaModule to UploadsModule imports

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './uploads.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UploadsController],
   providers: [UploadsService],
 })
